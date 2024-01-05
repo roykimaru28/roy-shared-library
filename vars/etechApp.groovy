53fde42b77f697821b61d6198a2815dba98177d2
Roy Kipkorir <roykipkorir@Roys-Air.attlocal.net>
2024-01-04 20:35:53 -0600

    new update

---
+++ vars/etechApp.groovy
@@ -0,0 +1,27 @@
+def call(string repoUrl){
+    pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'ls'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'pwd'
+               }
+           }
+       }
+}
+}
